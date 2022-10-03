c7b4ab3d6c02a58a13520b137b4854266b38156b
Vova <57757778+whatispain@users.noreply.github.com>
2022-10-03 15:00:59 +0300

    Lab 1

---
+++ WebService1.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoapFlab
+{
+    /// <summary>
+    /// Сводное описание для WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod(BufferResponse = true, CacheDuration = 0, EnableSession = false)]
+        public string MakeSyllable(string word)
+        {
+            return word.SyllableMaker();
+        }
+
+        
+    }
+}
